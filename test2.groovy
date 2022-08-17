b81b62651a00d8bc1d836c1e7eed6d09b39d3277
RanjanGitHubb <96113106+RanjanGitHubb@users.noreply.github.com>
2022-08-17 14:14:26 +0530

    Create test2.groovy

---
+++ test2.groovy
@@ -0,0 +1,14 @@
+node {
+  stage('Clone') {
+  git "https://github.com/RanjanGitHubb/mark2.git"
+  }
+  stage('Build') {
+    bat "mvn clean install"
+  }
+  stage('SonarCloud Analysis') {
+    withSonarQubeEnv('SonarCloud')
+    bat "mvn clean package sonar:sonar"
+    
+  }
+
+}
