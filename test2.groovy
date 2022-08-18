6aaf3a1108756c237f76411d17d8d564cfc8cac5
RanjanGitHubb <96113106+RanjanGitHubb@users.noreply.github.com>
2022-08-18 17:05:57 +0530

    Update test2.groovy

--- test2.groovy
+++ test2.groovy
@@ -6,7 +6,8 @@ node {
     bat "mvn -Dmaven.test.failure.ignore=true clean install"
   }
   stage('SonarCloud Analysis') {
-    bat "mvn verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dsonar.projectKey=RanjanGitHubb_mark2"
+    bat "mvn verify sonar:sonar"
+    //bat "mvn verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dsonar.projectKey=RanjanGitHubb_mark2"
   }
 
 }
