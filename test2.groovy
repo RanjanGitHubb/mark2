56b833c1fd3add92ecc9edd4085c54ea013e28be
RanjanGitHubb <96113106+RanjanGitHubb@users.noreply.github.com>
2022-08-18 21:44:30 +0530

    Update test2.groovy

--- test2.groovy
+++ test2.groovy
@@ -5,9 +5,24 @@ node {
   stage('Build') {
     bat "mvn -Dmaven.test.failure.ignore=true clean package"
   }
-  stage('SonarCloud Analysis') {
-    bat "mvn verify sonar:sonar"
+  stage('upload to nexus') {
+    //bat "mvn verify sonar:sonar"
     //bat "mvn verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dsonar.projectKey=RanjanGitHubb_mark2"
+    nexusArtifactUploader artifacts: [
+      [
+        artifactId: 'mavenforjenkins', 
+        classifier: '', 
+        file: 'target/mavenforjenkins-0.0.1-SNAPSHOT.jar', 
+        type: 'jar'
+      ]
+    ], 
+      credentialsId: '08b1f4bf-c897-4b47-9d27-82c26e6561a8', 
+      groupId: 'mavenforjenkins', 
+      nexusUrl: 'localhost:8081', 
+      nexusVersion: 'nexus3', 
+      protocol: 'http', 
+      repository: 'gs-maven', 
+      version: '0.0.1-SNAPSHOT
   }
 
 }
