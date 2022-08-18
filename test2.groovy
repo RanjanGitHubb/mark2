d159680b9c95e1794db6ee85ab7f25304217948d
RanjanGitHubb <96113106+RanjanGitHubb@users.noreply.github.com>
2022-08-18 22:19:46 +0530

    Update test2.groovy

--- test2.groovy
+++ test2.groovy
@@ -6,9 +6,14 @@ node {
     bat "mvn -Dmaven.test.failure.ignore=true clean package"
     archiveArtifacts artifacts: 'target/*.jar', onlyIfSuccessful: true
   }
+  stage ('analysis') {
+    bat "mvn verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar"
+    junit 'target/surefire-reports/*.xml'
+    
+  }
   stage('upload to nexus') {
     //bat "mvn verify sonar:sonar"
-    //bat "mvn verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dsonar.projectKey=RanjanGitHubb_mark2"
+    // bat "mvn verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dsonar.projectKey=RanjanGitHubb_mark2"
    
       nexusArtifactUploader artifacts: [
       [
