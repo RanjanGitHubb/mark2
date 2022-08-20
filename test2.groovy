1cbd5ec75668baad2107aab7cb25c1489170cca0
RanjanGitHubb <96113106+RanjanGitHubb@users.noreply.github.com>
2022-08-20 19:07:48 +0530

    Update test2.groovy

--- test2.groovy
+++ test2.groovy
@@ -7,10 +7,10 @@ node {
     //archiveArtifacts artifacts: 'target/*.jar', onlyIfSuccessful: true
   }
   stage ('analysis') {
-    //bat "mvn verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar"
+    //bat "mvn verify sonar:sonar \"
     //junit 'target/surefire-reports/*.xml'
     //bat "mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar \
-    bat "mvn verify sonar:sonar \
+    bat "mvn org.sonarsource.scanner.maven:sonar-maven-plugin:sonar \
     -Dsonar.projectKey=gs-maven1 \
     -Dsonar.host.url=http://localhost:9000 \
     -Dsonar.user=admin \
